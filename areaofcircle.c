41e798f1e7903c700f6a0b64b7402d806817cf28
dishita-222 <ds2890@srmist.edu.in>
2023-03-25 21:21:09 +0530

    commit

---
+++ areaofcircle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float r;
+    printf("Enter the radius of circle: ");
+    scanf("%f",&r);
+    float area = 3.14 * r * r;
+    printf("The area of circle is : %f",area);
+    return 0;
+
+
+}
---
+++ areaofsq.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("The side of square is ");
+    scanf("%d",&a);
+    int area=a*a;
+    printf("The area is %d",area);
+    return 0 ;
+    
+}
